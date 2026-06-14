b5eac86bd7d6e97b68c627d0d4fb23551449d5d9
kashish <gkashish005@gmail.com>
2026-06-14 15:15:12 +0530

    Initial Spring Boot setup

---
+++ src/main/java/com/kashish/astrologer_crm/AstrologerCrmApplication.java
@@ -0,0 +1,13 @@
+package com.kashish.astrologer_crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstrologerCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstrologerCrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kashish/astrologer_crm/AstrologerCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kashish.astrologer_crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstrologerCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
